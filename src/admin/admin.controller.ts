ecef32bee2a90feaeede80683d6400325b07d864
PlanHorror <nmtxma2004@gmail.com>
2025-09-28 17:12:07 +0700

    feat: index for guard

--- src/admin/admin.controller.ts
+++ src/admin/admin.controller.ts
@@ -1,6 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, UseGuards } from '@nestjs/common';
 import { AdminService } from './admin.service';
+import { AuthGuard } from '@nestjs/passport';
+import { RoleGuard } from 'common/guard/role.guard';
+import { Role } from 'common';
 
+@UseGuards(AuthGuard('admin-jwt'), new RoleGuard([Role.ADMIN]))
 @Controller('admin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
