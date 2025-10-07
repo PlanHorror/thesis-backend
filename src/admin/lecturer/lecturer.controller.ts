6ab346fdcbc0561a3f881362a0798f2423593468
PlanHorror <nmtxma2004@gmail.com>
2025-10-08 06:39:24 +0700

    feat: admin manage lecturer endpoint

--- src/admin/lecturer/lecturer.controller.ts
+++ src/admin/lecturer/lecturer.controller.ts
@@ -1,4 +1,59 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Role } from 'common/enum/role.enum';
+import { RoleGuard } from 'common/guard/role.guard';
+import { AdminService } from '../admin.service';
+import {
+  CreateLecturerDto,
+  CreateMultipleLecturersDto,
+  UpdateLecturerDto,
+} from '../dto/lecturer.dto';
 
+@UseGuards(AuthGuard('accessToken'), new RoleGuard([Role.ADMIN]))
 @Controller('admin/lecturer')
-export class LecturerController {}
+export class LecturerController {
+  constructor(private readonly adminService: AdminService) {}
+
+  @Get('all')
+  async findAll() {
+    return this.adminService.getAllLecturerAccountsService();
+  }
+
+  @Get('find/:id')
+  async findOne(@Param('id') id: string) {
+    return this.adminService.getLecturerAccountByIdService(id);
+  }
+
+  @Post('create')
+  async create(@Body() data: CreateLecturerDto) {
+    return this.adminService.createLecturerAccountService(data);
+  }
+  @Post('create/multiple')
+  async createMultiple(@Body() data: CreateMultipleLecturersDto) {
+    return this.adminService.createMultipleLecturerAccountsService(data);
+  }
+
+  @Patch('update/:id')
+  async update(@Param('id') id: string, @Body() data: UpdateLecturerDto) {
+    return this.adminService.updateLecturerAccountService(id, data);
+  }
+
+  @Delete('delete/:id')
+  async delete(@Param('id') id: string) {
+    return this.adminService.deleteLecturerAccountService(id);
+  }
+
+  @Delete('delete')
+  async deleteMultiple(@Body('ids') ids: string[]) {
+    return this.adminService.deleteMultipleLecturerAccountsService(ids);
+  }
+}
