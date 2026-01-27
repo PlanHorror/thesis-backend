418a8acf2ff057b94e1e4262a2d44eb75635407e
PlanHorror <nmtxma2004@gmail.com>
2026-01-27 18:38:21 +0700

    feat: semester endpoint

--- src/semester/course-semester/course-semester.controller.ts
+++ src/semester/course-semester/course-semester.controller.ts
@@ -1,4 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { CourseSemesterService } from './course-semester.service';
+import { isBoolean } from 'class-validator';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('accessToken'))
 @Controller('course-semester')
-export class CourseSemesterController {}
+export class CourseSemesterController {
+  constructor(private readonly courseSemesterService: CourseSemesterService) {}
+
+  @Get('all')
+  async getAllCourseSemesters(
+    @Query('includeCourses') includeCourses?: boolean,
+    @Query('includeSemesters') includeSemesters?: boolean,
+    @Query('courseId') courseId?: string,
+    @Query('semesterId') semesterId?: string,
+  ) {
+    return await this.courseSemesterService.findAll(
+      isBoolean(includeCourses) ? includeCourses : false,
+      isBoolean(includeSemesters) ? includeSemesters : false,
+      courseId,
+      semesterId,
+    );
+  }
+
+  @Get('find/:id')
+  async getCourseSemesterById(
+    @Param('id') id: string,
+    @Query('includeCourses') includeCourses?: boolean,
+    @Query('includeSemesters') includeSemesters?: boolean,
+  ) {
+    return await this.courseSemesterService.findOne(
+      id,
+      isBoolean(includeCourses) ? includeCourses : false,
+      isBoolean(includeSemesters) ? includeSemesters : false,
+    );
+  }
+}
--- src/semester/semester.controller.ts
+++ src/semester/semester.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query, UseGuards } from '@nestjs/common';
+import { SemesterService } from './semester.service';
+import { isBoolean } from 'class-validator';
+import { AuthGuard } from '@nestjs/passport';
 
+@UseGuards(AuthGuard('accessToken'))
 @Controller('semester')
-export class SemesterController {}
+export class SemesterController {
+  constructor(private readonly semesterService: SemesterService) {}
+
+  @Get('all')
+  async getAllSemesters(
+    @Query('includeCourses') includeCourses?: boolean,
+    @Query('includeDocuments') includeDocuments?: boolean,
+  ) {
+    return await this.semesterService.findAll(
+      isBoolean(includeCourses) ? includeCourses : false,
+      isBoolean(includeDocuments) ? includeDocuments : false,
+    );
+  }
+
+  @Get('find/:id')
+  async getSemesterById(
+    @Param('id') id: string,
+    @Query('includeCourses') includeCourses?: boolean,
+    @Query('includeDocuments') includeDocuments?: boolean,
+  ) {
+    return await this.semesterService.findById(
+      id,
+      isBoolean(includeCourses) ? includeCourses : false,
+      isBoolean(includeDocuments) ? includeDocuments : false,
+    );
+  }
+}
