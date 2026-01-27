7bb9120326522f3366431bff3ee9e37ee5d3cbda
PlanHorror <nmtxma2004@gmail.com>
2026-01-27 13:25:11 +0700

    feat: get user decorator

--- src/course/course.controller.ts
+++ src/course/course.controller.ts
@@ -1,4 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { CourseService } from './course.service';
 
 @Controller('course')
-export class CourseController {}
+export class CourseController {
+  constructor(private readonly courseService: CourseService) {}
+
+  @Get('all')
+  async findAll() {
+    return this.courseService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.courseService.findOne(id);
+  }
+}
--- src/course/enrollment/enrollment.controller.ts
+++ src/course/enrollment/enrollment.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { EnrollmentService } from './enrollment.service';
 
 @Controller('enrollment')
-export class EnrollmentController {}
+export class EnrollmentController {
+  constructor(private readonly enrollmentService: EnrollmentService) {}
+
+  @Get('all')
+  async getAllEnrollments() {
+    return this.enrollmentService.findAll();
+  }
+}
