683fefcb8420954b8af8e5d05b7beab5f5784c73
PlanHorror <nmtxma2004@gmail.com>
2026-01-30 18:08:39 +0700

    feat: implement post for user

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,4 +1,44 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
+import { PostService } from './post.service';
 
 @Controller('post')
-export class PostController {}
+export class PostController {
+  constructor(private readonly postService: PostService) {}
+
+  @Get('global')
+  async getGlobalPosts(
+    @Query('includeAdmin') includeAdmin?: string,
+    @Query('includeDepartment') includeDepartment?: string,
+  ) {
+    return await this.postService.findGlobalPosts(
+      includeAdmin === 'true',
+      includeDepartment === 'true',
+    );
+  }
+
+  @Get('department/:departmentId')
+  async getPostsByDepartment(
+    @Param('departmentId') departmentId: string,
+    @Query('includeAdmin') includeAdmin?: string,
+    @Query('includeDepartment') includeDepartment?: string,
+  ) {
+    return await this.postService.findByDepartmentId(
+      departmentId,
+      includeAdmin === 'true',
+      includeDepartment === 'true',
+    );
+  }
+
+  @Get(':id')
+  async getPostById(
+    @Param('id') id: string,
+    @Query('includeAdmin') includeAdmin?: string,
+    @Query('includeDepartment') includeDepartment?: string,
+  ) {
+    return await this.postService.findById(
+      id,
+      includeAdmin === 'true',
+      includeDepartment === 'true',
+    );
+  }
+}
