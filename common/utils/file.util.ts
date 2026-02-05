ace73565c0ddaf9893414a5e403e6efcb5db8fad
L <lillustrer012@gmail.com>
2026-02-05 22:03:12 +0700

    update file utils

--- common/utils/file.util.ts
+++ common/utils/file.util.ts
@@ -1,9 +1,9 @@
-import { BadRequestException } from '@nestjs/common';
-import * as fs from 'fs';
+import { BadRequestException } from "@nestjs/common";
+import * as fs from "fs";
 
 export function generateFileName(file: Express.Multer.File): string {
   const timestamp = Date.now();
-  const originalName = file.originalname.replace(/\s+/g, '_');
+  const originalName = file.originalname.replace(/\s+/g, "_");
   return `${timestamp}_${originalName}`;
 }
 
@@ -14,8 +14,8 @@ export async function saveFile(
   try {
     await fs.promises.writeFile(path, file.buffer);
   } catch (error) {
-    console.error('Error saving file:', error);
-    throw new BadRequestException('Error saving file');
+    console.error("Error saving file:", error);
+    throw new BadRequestException("Error saving file");
   }
 }
 
@@ -23,7 +23,7 @@ export async function deleteFile(path: string): Promise<void> {
   try {
     await fs.promises.unlink(path);
   } catch (error) {
-    console.error('Error deleting file:', error);
-    throw new BadRequestException('Error deleting file');
+    console.error("Error deleting file:", error);
+    throw new BadRequestException("Error deleting file");
   }
 }
